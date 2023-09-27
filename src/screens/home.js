1f903f9c2132de4e6e1db80a44f33522f6fe388f
Luiz Henrique <lh.m21112000@gmail.com>
2023-09-27 06:59:32 -0300

    atualizacao

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,25 +1,97 @@
-import React from 'react';
-import { Text, StyleSheet,SafeAreaView } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Modal from 'react-native-modal';
 
 export default function Home() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>Tela das obras cadastradas</Text>
-        </SafeAreaView>
-    );
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.header} onPress={toggleModal}>
+        <Icon name="search" size={30} color="white" style={styles.searchIcon} />
+      </TouchableOpacity>
+      <Text style={styles.text}>Aqui as obras cadastradas serão mostradas</Text>
+      <View style={styles.footer}>
+        <Icon name="plus" size={30} color="white" style={styles.addIcon} />
+      </View>
+      <Modal isVisible={isModalVisible} onBackdropPress={toggleModal}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Pesquisar por nome</Text>
+          <TextInput
+            placeholder="Digite o nome"
+            style={styles.input}
+            placeholderTextColor="white"
+            color="white"
+          />
+          <TouchableOpacity onPress={toggleModal} style={styles.modalButton}>
+            <Text style={styles.modalButtonText}>Pesquisar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#454A50",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    text: {
-      fontSize: 20,
-      color: "white",
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#454A50",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    zIndex: 1,
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: "white",
+  },
+  footer: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    zIndex: 1,
+  },
+  addIcon: {
+    padding: 10,
+  },
+  modalContainer: {
+    backgroundColor: '#454A50',
+    padding: 20,
+    borderRadius: 8,
+  },
+  modalTitle: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  input: {
+    borderColor: '#FFF',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: 'grey',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+  },
+  modalButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
--- src/screens/user.js
+++ src/screens/user.js
@@ -1,46 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Image, Alert, TextInput, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { Button } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
-import ModalSenha from 'react-native-modal';
-import ModalEmail from 'react-native-modal'
-import ImagePicker from 'react-native-image-picker';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import ModalSenha from "react-native-modal";
+import ModalEmail from "react-native-modal";
+import * as ImagePicker from "expo-image-picker";
 
 export default function User() {
   const navigation = useNavigation();
   const [isModaSenhaVisible, setModalSenhaVisible] = useState(false);
-  const [emailSenha, setEmailSeha] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-
+  const [emailSenha, setEmailSeha] = useState("");
+  const [newPassword, setNewPassword] = useState("");
   const [avatarSource, setAvatarSource] = useState(null);
+  const [photoChanged, setPhotoChanged] = useState(false); // Estado para rastrear se a foto foi alterada
 
- 
-
-  const handleSelectImage = () => {
-    const options = {
-      title: 'Selecionar foto de perfil',
-      takePhotoButtonTitle: 'Tirar foto',
-      chooseFromLibraryButtonTitle: 'Escolher da galeria',
-      cancelButtonTitle: 'Cancelar',
+  const selectImageFromGallery = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
       quality: 1,
-      maxWidth: 500,
-      maxHeight: 500,
-    };
-
-    ImagePicker.showImagePicker(options, (response) => {
-      if (response.didCancel) {
-        console.log('Usuário cancelou a escolha da imagem');
-      } else if (response.error) {
-        console.error('Erro ao selecionar a imagem:', response.error);
-      } else {
-        // A imagem foi selecionada com sucesso
-        setAvatarSource({ uri: response.uri });
-      }
     });
+
+    if (!result.canceled) {
+      setAvatarSource(result.assets[0].uri);
+      setPhotoChanged(true); // Marcar a foto como alterada
+    }
   };
 
+  const handleSavePhoto = () => {
+    // Lógica para salvar a foto
+    Alert.alert(
+      "Foto salva com sucesso!",
+      "A foto foi salva com sucesso!",
+      [
+        {
+          text: "OK",
+        },
+      ],
 
-
+      setPhotoChanged(false)
+    );
+  };
 
   const handleLogout = () => {
     Alert.alert(
@@ -60,7 +73,7 @@ export default function User() {
             // Lógica de logout do usuário, remover token ou limpar o estado
 
             // Após encerrar sessão
-            navigation.navigate('Login');
+            navigation.navigate("Login");
           },
         },
       ],
@@ -86,26 +99,23 @@ export default function User() {
             // Lógica de logout do usuário, remover token ou limpar o estado
 
             // Após encerrar sessão
-            navigation.navigate('Login');
+            navigation.navigate("Login");
           },
         },
-      ],
+      ]
     );
     // Feche o modal após salvar
     toggleModalSenha();
   };
 
-
-
   // estrutura de moral para redefinir email
   const [isModaEmailVisible, setModalEmailVisible] = useState(false);
-  const [email, setEmail] = useState('');
-  const [newEmail, setNewEmail] = useState('');
+  const [email, setEmail] = useState("");
+  const [newEmail, setNewEmail] = useState("");
 
   const toggleModalEmail = () => {
     setModalEmailVisible(!isModaEmailVisible);
   };
-
 
   const handleSaveEmail = () => {
     Alert.alert(
@@ -114,31 +124,42 @@ export default function User() {
       [
         {
           text: "Confirmar",
-          onPress: () => {
-
-          },
+          onPress: () => {},
         },
-      ],
+      ]
     );
     // Lógica para salvar o novo email
-
 
     // Feche o modal após salvar
     toggleModalEmail();
   };
 
-
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity onPress={()=>{}}>
-          <Image
-            source={avatarSource ? avatarSource : require('../../assets/bgImage.jpg')}
-            style={styles.userImage}
-          />
+        <Image
+          source={
+            avatarSource
+              ? { uri: avatarSource }
+              : require("../../assets/undefinedUser.jpg")
+          }
+          style={styles.userImage}
+        />
+        <TouchableOpacity onPress={selectImageFromGallery}>
+          <Text style={styles.changeImageButton}>Trocar Imagem</Text>
         </TouchableOpacity>
+        {photoChanged && ( // Mostrar o botão "Salvar Foto" somente se a foto foi alterada
+          <TouchableOpacity onPress={handleSavePhoto}>
+            <Text style={styles.changeImageButton}>Salvar Foto</Text>
+          </TouchableOpacity>
+        )}
+
+        <View style={styles.userData}>
+          <Text style={styles.userDataText}>Usuário: userName</Text>
+          <Text style={styles.userDataText}>E-mail: example@email.com</Text>
+        </View>
       </View>
+
       <View style={styles.buttonContainer}>
         <Button
           title="Redefinir senha"
@@ -182,17 +203,22 @@ export default function User() {
                 placeholderTextColor="white"
                 color="white"
               />
-              <TouchableOpacity onPress={handleSavePassword} style={styles.modalButton}>
+              <TouchableOpacity
+                onPress={handleSavePassword}
+                style={styles.modalButton}
+              >
                 <Text style={styles.modalButtonText}>Salvar</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={toggleModalSenha} style={styles.modalButton}>
+              <TouchableOpacity
+                onPress={toggleModalSenha}
+                style={styles.modalButton}
+              >
                 <Text style={styles.modalButtonText}>Cancelar</Text>
               </TouchableOpacity>
             </View>
           </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
       </ModalSenha>
-
 
       <ModalEmail isVisible={isModaEmailVisible}>
         <KeyboardAvoidingView
@@ -218,78 +244,94 @@ export default function User() {
                 placeholderTextColor="white"
                 color="white"
               />
-              <TouchableOpacity onPress={handleSaveEmail} style={styles.modalButton}>
+              <TouchableOpacity
+                onPress={handleSaveEmail}
+                style={styles.modalButton}
+              >
                 <Text style={styles.modalButtonText}>Salvar</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={toggleModalEmail} style={styles.modalButton}>
+              <TouchableOpacity
+                onPress={toggleModalEmail}
+                style={styles.modalButton}
+              >
                 <Text style={styles.modalButtonText}>Cancelar</Text>
               </TouchableOpacity>
             </View>
           </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
       </ModalEmail>
-
     </SafeAreaView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#454A50",
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingTop: 20,
   },
   userImage: {
     width: 200,
     height: 200,
     borderRadius: 100,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderWidth: 8,
-    borderColor: 'grey',
+    borderColor: "grey",
     padding: 4,
   },
+  changeImageButton: {
+    color: "white",
+    marginTop: 5,
+  },
+  userDataText: {
+    color: "white",
+    fontSize: 16,
+  },
+  userData: {
+    marginTop: 15,
+    alignItems: 'center'
+  },
   buttonContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   button: {
-    backgroundColor: 'grey', // Cor de fundo dos botões
+    backgroundColor: "grey", // Cor de fundo dos botões
     width: 200,
     marginVertical: 10,
     borderRadius: 10,
   },
   modalContainer: {
-    backgroundColor: '#454A50',
+    backgroundColor: "#454A50",
     padding: 20,
     borderRadius: 8,
   },
   modalTitle: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   input: {
-    borderColor: '#FFF',
+    borderColor: "#FFF",
     borderWidth: 1,
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
   },
   modalButton: {
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     borderRadius: 5,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
   modalButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
 });
