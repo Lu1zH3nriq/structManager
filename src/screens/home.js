7b35e6a3c9e7c59a7cd1bd0457743a9e902d09c4
Luiz Henrique <lh.m21112000@gmail.com>
2023-09-27 22:08:09 -0300

    atualizacao

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import ModalPesquisa from 'react-native-modal';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import ModalPesquisa from "react-native-modal";
+import ModalCadastro from "react-native-modal";
+import { TextInputMask } from "react-native-masked-text";
 
 export default function Home() {
   const [isModalPesquisaVisible, setModalPesquisaVisible] = useState(false);
+  const [isModalCadastroVisible, setModalCadastroVisible] = useState(false);
+  const [nomeCliente, setNomeCliente] = useState("");
+  const [tipoObra, setTipoObra] = useState("");
+  const [dataInicio, setDataInicio] = useState("");
 
   const toggleModalPesquisa = () => {
     setModalPesquisaVisible(!isModalPesquisaVisible);
+  };
+
+  const toggleModalCadastro = () => {
+    setModalCadastroVisible(!isModalCadastroVisible); // Função para abrir/fechar o modal de cadastro
+  };
+
+  //função para pesquisar as obras
+  const handlePesquisar = () => {
+    // Lógica de pesquisa através dos campos informados aqui
+
+    //ao final fechar o modal de pesquisa
+    toggleModalPesquisa();
   };
 
   return (
@@ -15,34 +42,154 @@ export default function Home() {
       <TouchableOpacity style={styles.header} onPress={toggleModalPesquisa}>
         <Icon name="search" size={30} color="white" style={styles.searchIcon} />
       </TouchableOpacity>
+
       <Text style={styles.text}>Aqui as obras cadastradas serão mostradas</Text>
+
       <View style={styles.footer}>
-        <Icon name="plus" size={30} color="white" style={styles.addIcon} />
+        <TouchableOpacity onPress={toggleModalCadastro}>
+          <Icon name="plus" size={30} color="white" style={styles.addIcon} />
+        </TouchableOpacity>
       </View>
 
-
-
-
-
-
-      <ModalPesquisa isVisible={isModalPesquisaVisible} onBackdropPress={toggleModalPesquisa}>
+      <ModalPesquisa
+        isVisible={isModalPesquisaVisible}
+        onBackdropPress={toggleModalPesquisa}
+      >
         <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : undefined}
         >
           <View style={styles.modalContainer}>
-            <Text style={styles.modalTitle}>Pesquisar obra</Text>
+            <Text style={styles.modalTitle}>Pesquisar obras</Text>
             <TextInput
               placeholder="Nome do Cliente"
               style={styles.input}
               placeholderTextColor="white"
               color="white"
+              value={nomeCliente}
+              onChangeText={(text) => setNomeCliente(text)}
             />
-            <TouchableOpacity onPress={toggleModalPesquisa} style={styles.modalButton}>
+
+            <TextInput
+              placeholder="Tipo de Obra"
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+              value={tipoObra}
+              onChangeText={(text) => setTipoObra(text)}
+            />
+            <TextInputMask
+              type={"datetime"}
+              options={{
+                format: "DD/MM/YYYY",
+              }}
+              placeholder="Data de Início"
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+              value={dataInicio}
+              onChangeText={(text) => setDataInicio(text)}
+              keyboardType="numeric"
+            />
+
+            <TouchableOpacity
+              onPress={handlePesquisar}
+              style={styles.modalButton}
+            >
               <Text style={styles.modalButtonText}>Pesquisar</Text>
             </TouchableOpacity>
           </View>
         </KeyboardAvoidingView>
       </ModalPesquisa>
+
+      <ModalCadastro
+        isVisible={isModalCadastroVisible}
+        onBackdropPress={toggleModalCadastro}
+      >
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Cadastrar nova obra</Text>
+            {/* Adicione os campos e a lógica para cadastrar uma nova obra aqui */}
+            <Icon
+              name="search"
+              size={30}
+              color="white"
+              style={styles.searchIconCadastro}
+            />
+            <TextInput
+              placeholder="Cliente: "
+              style={styles.inputSearch}
+              placeholderTextColor="white"
+              color="white"
+            />
+
+            <TextInput
+              placeholder="Contato: "
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+            />
+
+            <TextInput
+              placeholder="Tipo de Obra: "
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+            />
+
+            <TextInput
+              placeholder="Endereço da Obra: "
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+            />
+
+            <TextInputMask
+              type={"datetime"}
+              options={{
+                format: "DD/MM/YYYY",
+              }}
+              placeholder="Data de Início:"
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+              keyboardType="numeric"
+            />
+
+            <TextInputMask
+              type={"datetime"}
+              options={{
+                format: "DD/MM/YYYY",
+              }}
+              placeholder="Data prevista de Término:"
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+              keyboardType="numeric"
+            />
+
+            <TextInput
+              type={"courency"}
+              options={{
+                format: "R$9.999,99",
+              }}
+              placeholder="Orçamento: "
+              style={styles.input}
+              placeholderTextColor="white"
+              color="white"
+              keyboardType="numeric"
+            />
+
+            <TouchableOpacity
+              onPress={toggleModalCadastro}
+              style={styles.modalButton}
+            >
+              <Text style={styles.modalButtonText}>Cadastrar</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </ModalCadastro>
     </SafeAreaView>
   );
 }
@@ -56,12 +203,18 @@ const styles = StyleSheet.create({
   },
   header: {
     position: "absolute",
-    top: '5%',
-    left: 10,
+    top: "5%",
+    left: '3%',
     zIndex: 1,
   },
   searchIcon: {
     padding: 10,
+  },
+  searchIconCadastro: {
+    position: "absolute",
+    right: "10%",
+    top: "13.5%",
+    zIndex: 1,
   },
   text: {
     fontSize: 20,
@@ -69,39 +222,48 @@ const styles = StyleSheet.create({
   },
   footer: {
     position: "absolute",
-    bottom: 10,
-    right: 10,
+    bottom: '3%',
+    right: '5%',
     zIndex: 1,
   },
   addIcon: {
     padding: 10,
   },
   modalContainer: {
-    backgroundColor: '#454A50',
+    backgroundColor: "#454A50",
     padding: 20,
     borderRadius: 8,
   },
   modalTitle: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginBottom: 20,
   },
   input: {
-    borderColor: '#FFF',
+    borderColor: "#FFF",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+  },
+  inputSearch: {
+    borderColor: "#FFF",
     borderWidth: 1,
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
+    width: "80%",
   },
   modalButton: {
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     borderRadius: 5,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   modalButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
 });
