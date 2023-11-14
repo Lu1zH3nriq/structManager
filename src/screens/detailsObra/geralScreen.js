38bc3813ab19ec5763513af680136be4a6758d6b
Luiz Henrique <lh.m21112000@gmail.com>
2023-11-14 11:25:30 -0300

    atualizacao nos estilos da tela geralscreen

--- src/screens/detailsObra/geralScreen.js
+++ src/screens/detailsObra/geralScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { Text, StyleSheet, SafeAreaView, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+import materialScreen from "./materialScreen";
+import funcionarioScreen from "./funcionarioScreen";
+import equipamentoScreen from "./equipamentoScreen.js";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -9,7 +19,7 @@ export default function GeralScreen() {
   const navigation = useNavigation();
   const route = useRoute();
 
-  // Obtenha os detalhes da obra dos parâmetros
+  // Obtendo os detalhes da obra do parâmetro
   const obra = route.params?.obra || {};
 
   // Função para navegar para outras telas relacionadas à obra
@@ -18,14 +28,57 @@ export default function GeralScreen() {
     navigation.navigate(tela, { obra });
   };
 
+  const [cliente, setCliente] = React.useState("");
+
+  React.useEffect(() => {
+    const getCliente = async () => {
+      try {
+        let baseUrl = "http://192.168.100.3:3000/buscaCliente";
+        let params = { id: obra.clienteId };
+        let url = `${baseUrl}?${new URLSearchParams(params).toString()}`;
+
+        let response = await fetch(url, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setCliente(data.nome);
+        } else {
+          console.error("Erro na requisição: ", response.status);
+        }
+      } catch (error) {
+        console.error("Erro na requisição: ", error);
+      }
+    };
+
+    getCliente();
+  }, []);
+
+  const TelaMateriais = () => <materialScreen />;
+  const TelaFuncionarios = () => <funcionarioScreen />;
+  const TelaEquipamentos = () => <equipamentoScreen />;
+
   return (
     <SafeAreaView style={styles.container}>
-      {/* Renderize os detalhes da obra (por exemplo, nome da obra) */}
-      <Text style={{ color: "white" }}>Nome da Obra: {obra.nome}</Text>
-      <Text style={{ color: "white" }}>Tipo: {obra.tipo}</Text>
-      <Text style={{ color: "white" }}>Data de Início: {obra.dataInicio}</Text>
+      {/* Envolve os detalhes da obra em uma View com estilos específicos */}
+      <View style={styles.detalhesObraContainer}>
+        <Text style={styles.textoDetalhes}>Nome da Obra: {obra.nome}</Text>
+        <Text style={styles.textoDetalhes}>Tipo: {obra.tipo}</Text>
+        <Text style={styles.textoDetalhes}>
+          Data de Início: {obra.dataInicio}
+        </Text>
+        <Text style={styles.textoDetalhes}>
+          Data prevista para Término: {obra.dataFim}
+        </Text>
+        <Text style={styles.textoDetalhes}>Cliente da Obra: {cliente}</Text>
+      </View>
 
-      {/* Renderize a barra de navegação para outras telas relacionadas à obra */}
+      {/* Renderiza a barra de navegação para outras telas relacionadas à obra */}
       <View style={styles.barraNavegacao}>
         <Tab.Navigator
           screenOptions={{
@@ -37,54 +90,31 @@ export default function GeralScreen() {
             },
           }}
         >
-          <Tab.Screen
-            name="TelaRelacionada1"
-            component={TelaRelacionada1}
-          />
-          <Tab.Screen
-            name="TelaRelacionada2"
-            component={TelaRelacionada2}
-          />
-          <Tab.Screen
-            name="TelaRelacionada3"
-            component={TelaRelacionada3}
-          />
+          <Tab.Screen name="Materiais" component={TelaMateriais} />
+          <Tab.Screen name="Funcionários" component={TelaFuncionarios} />
+          <Tab.Screen name="Equipamentos" component={TelaEquipamentos} />
         </Tab.Navigator>
       </View>
     </SafeAreaView>
   );
 }
-
-const TelaRelacionada1 = () => (
-  <View style={styles.telaRelacionada}>
-    <Text style={{ color: "white" }}>Tela Relacionada 1</Text>
-  </View>
-);
-
-const TelaRelacionada2 = () => (
-  <View style={styles.telaRelacionada}>
-    <Text style={{ color: "white" }}>Tela Relacionada 2</Text>
-  </View>
-);
-
-const TelaRelacionada3 = () => (
-  <View style={styles.telaRelacionada}>
-    <Text style={{ color: "white" }}>Tela Relacionada 3</Text>
-  </View>
-);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#454A50",
+    backgroundColor: "#F0F0F0", // Cor de fundo diferente
+    padding: 5, // Padding de 5px
+    borderRadius: 10, // Bordas arredondadas
   },
   barraNavegacao: {
     flexDirection: "row",
     marginTop: 20,
   },
-  telaRelacionada: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  detalhesObraContainer: {
+    margin: 5, // Margem de 5px do topo e da esquerda
+  },
+  textoDetalhes: {
+    color: "white",
+    marginBottom: 3, // Espaçamento de 3px entre cada Text
+    fontWeight: "bold", // Fonte em negrito
   },
 });
